0bd09163bc4b2d6c05dcc0c9da627a311e3f74b0
MrNamra <namraramsha@yahoo.com>
2025-07-26 15:42:38 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const [star,setStar] = useState (100);
   const handleClick = () => {
-    setCount(count + 1); 
+    if (count < 25) {
+      setCount(count +1);
+    }
+    if( count === 10){
+      setStar(200)
+    }else {
+      setStar(0)
+    }
   };
-   const handleClick2 = () => {
-    setCount(count - 1);
-   }
+  const handleClick2 = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
   return (
     <>
       <h4>LEARNING REACT</h4>
-      <p>Count: {count}</p> 
+      <p>Count: {count}</p>
       <button onClick={handleClick}>+</button>
       <button onClick={handleClick2}>-</button>
+      <p>Star : {star}</p>
       <h3>THANK YOU @</h3>
     </>
   );
