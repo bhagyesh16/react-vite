f27ce190cadc9ec4c55a686ef6ff7fcc27f28c29
MrNamra <namraramsha@yahoo.com>
2025-07-26 15:22:36 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ function App() {
   const handleClick = () => {
     setCount(count + 1); 
   };
-
+   const handleClick2 = () => {
+    setCount(count - 1);
+   }
   return (
     <>
       <h4>LEARNING REACT</h4>
       <p>Count: {count}</p> 
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={handleClick}>+</button>
+      <button onClick={handleClick2}>-</button>
       <h3>THANK YOU @</h3>
     </>
   );
